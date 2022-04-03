ca9db01f078f284641a4ec2819f7f0062d9a0c5e
musis <feativa@gmail.com>
2022-04-02 22:55:15 -0300

    New Commit

--- lib/data/genre_model.dart
+++ lib/data/genre_model.dart
@@ -2,12 +2,13 @@ class GenreModel {
   int id;
   String name;
 
-  GenreModel({required this.id, required this.name,});
+  GenreModel({
+    required this.id,
+    required this.name,
+  });
 
-  factory GenreModel.fromJson(Map<String, dynamic> json) {
-    return GenreModel(
-      id: json['id'],
-      name: json['name'],
-    );
-  }
+  factory GenreModel.fromJson(Map<String, dynamic> json) => GenreModel(
+        id: json['id'],
+        name: json['name'],
+      );
 }
